03f16be65bb410acd4840990606bdcfca4329979
Trickster2001 <keyurmurkar@gmail.com>
2024-03-15 18:16:04 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+
+let main = document.querySelector("#main");
+let crsr = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (dets)=>{
+  // console.log(dets)
+  crsr.style.left = dets.clientX + "px";
+  crsr.style.top = dets.clientY + "px";
+})
